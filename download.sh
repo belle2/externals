a5efa6a469f669c7f55448a78b33d9e1451e7ed2
Thomas Kuhr <Thomas.Kuhr@lmu.de>
2012-08-15 08:04:53 +0000

    support zip files
    git-svn-id: https://belle2.cc.kek.jp/svn/trunk/externals@3537 b38c6c8f-bee8-465d-8448-9abe6915d496

--- download.sh
+++ download.sh
@@ -14,6 +14,7 @@ RESULT=1
 while [ $# -gt 0 ]; do
   URL=$1
   PROTOCOL=`echo ${URL} | awk -F: '{print $1}'`
+  EXTENSION=`echo ${URL} | awk -F. '{print $NF}'`
   shift
   if [ "${PROTOCOL}" = "svn" ]; then
     COMMAND=`echo ${URL} | awk -F: '{print $2}'`
@@ -21,7 +22,15 @@ while [ $# -gt 0 ]; do
     LINK=`echo ${URL} | sed 's;svn:\w*:\w*:;;'`
     svn ${COMMAND} -r${REVISION} ${LINK}
   else
-    wget -O - --tries=3 ${URL} | tar xz
+    if [ "${EXTENSION}" = "zip" ]; then
+      tmp=`mktemp  /tmp/belle2_tmp.XXXX`
+      rm -f $tmp
+      wget -O $tmp --tries=3 ${URL}
+      unzip $tmp
+      rm -f $tmp
+    else
+      wget -O - --tries=3 ${URL} | tar xz
+    fi
   fi
   RESULT=$?
   if [ "${RESULT}" = "0" ]; then
@@ -31,7 +40,7 @@ done
 
 # if none succeeded use the Belle II web server
 if [ "${RESULT}" -ne "0" ]; then
-  wget -O - --no-check-certificate --user=belle2 --password=Aith4tee https://belle2.cc.kek.jp/download/${FILE_NAME} | tar xz
+  wget -O - --tries=3 --no-check-certificate --user=belle2 --password=Aith4tee https://belle2.cc.kek.jp/download/${FILE_NAME} | tar xz
   RESULT=$?
 fi
 

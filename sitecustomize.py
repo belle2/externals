3a9964d0d323e4367a6cc5f9c7d54a39fecee20a
Martin Ritter <martin.ritter@desy.de>
2017-08-07 10:28:28 +0200

    Work around bug in virtualenv
    Apparently virtualenv ships a severely outdated site.py
    (https://github.com/pypa/virtualenv/issues/355) so our sitecustomize.py
    doesn't work with it.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,11 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6m
 
 import os
 import site
+try:
+    from site import getsitepackages
+except ImportError:
+    import sys
+
+    def getsitepackages(prefixes):
+        """Minimal copy of site.getsitepackages due to https://github.com/pypa/virtualenv/issues/355"""
+        sitepackages = []
+
+        for prefix in prefixes:
+            if os.sep == '/':
+                sitepackages.append(os.path.join(prefix, "lib",
+                                                 "python%d.%d" % sys.version_info[:2],
+                                                 "site-packages"))
+            else:
+                sitepackages.append(prefix)
+                sitepackages.append(os.path.join(prefix, "lib", "site-packages"))
+
+        return sitepackages
 
 # make sure the corresponding opt/debug part of the externals is also in the path
 if "BELLE2_EXTERNALS_DIR" in os.environ:
-    for site_dir in site.getsitepackages([os.path.expandvars("$BELLE2_EXTERNALS_DIR/$BELLE2_EXTERNALS_SUBDIR")]):
+
+    for site_dir in getsitepackages([os.path.expandvars("$BELLE2_EXTERNALS_DIR/$BELLE2_EXTERNALS_SUBDIR")]):
         if os.path.exists(site_dir):
             site.addsitedir(site_dir)
 

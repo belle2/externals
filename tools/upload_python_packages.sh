b40b53a79e0859f306a5a8357b4932bfdec78259
Frank Meier <frank.meier@duke.edu>
2024-03-07 14:55:45 -0500

    update download location on software server for python packages

--- tools/upload_python_packages.sh
+++ tools/upload_python_packages.sh
@@ -5,4 +5,4 @@ cd ..
 rm -fr src/python-packages .pip-certchain.pem
 make python-packages.src root-python-packages.src
 cd src/python-packages
-rsync -avzP . software.belle2.org:/var/www/html/download/python-packages
+rsync -avzP . software.belle2.org:/belle2/download/python-packages
